9c4282e4e6844ffbcdc0af59005b6d1d5ef1be2a
Prem Khanal <pkhanal@Prems-MacBook-Pro.local>
2014-04-13 08:38:50 -0400

    my commits

---
+++ fileTwo.js
@@ -0,0 +1,19 @@
+/**
+ * @author Prem Khanal
+ */
+
+
+$(document).ready(function)() {
+setNav();
+});
+
+function setNav() {
+	
+	$(".Pokhara").on("click", function() {
+		
+		$('#myModal').modal();
+		$(".model-body").html(video):
+	});
+	
+	
+}
